49fcc39f1d27b74fe1eeb93248eb734d8e3c8ce9
Nikolay Favorskiy <lordnikolas9091@gmail.com>
2022-01-11 15:40:15 +0100

    Update Game.ts

--- src/Game.ts
+++ src/Game.ts
@@ -11,4 +11,18 @@ export default class Game {
   public constructor(canvas: HTMLElement) {
     this.map = new Map(canvas);
   }
+
+  /**
+   * Game launch
+   */
+  public launchGame(): void {
+    this.map.renderMap();
+  }
+
+  /**
+   * Rendering of a frame
+   */
+  public renderFrame(): void {
+
+  }
 }
