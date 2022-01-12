3463660fd336c406e4eb129c2b6ed3a01738a4a8
Zapplightforce <pers0010@hz.nl>
2022-01-12 11:34:31 +0100

    refractored map

--- src/Game.ts
+++ src/Game.ts
@@ -3,26 +3,79 @@ import Map from './Map.js';
 export default class Game {
   private map: Map;
 
+  private canvas: HTMLCanvasElement;
+
+  private canvasContext : CanvasRenderingContext2D;
+
+  private currentSecond = 0;
+
+  private framesLastSecond = 0;
+
+  private frameCount = 0;
+
+  private lastUpdate = Date.now();
+
   /**
    * Construc a new instance of this class
    *
    * @param canvas the canvas to render on
    */
   public constructor(canvas: HTMLElement) {
-    this.map = new Map(canvas);
+    this.canvas = <HTMLCanvasElement>canvas;
+    this.canvas.width = window.innerWidth - 1;
+    this.canvas.height = window.innerHeight - 4;
   }
 
   /**
    * Game launch
    */
-  public launchGame(): void {
-    this.map.renderMap();
+  public gamelaunch(): void {
+    this.map = new Map(this.canvas, this.canvasContext);
+
+    requestAnimationFrame(() => this.renderFrame());
+  }
+
+  /**
+   * get canvas context
+   *
+   * @returns Context of the Canvas
+   */
+  public getCanvasContext(): CanvasRenderingContext2D {
+    this.canvasContext = this.canvas.getContext('2d');
+    return this.canvasContext;
+  }
+
+
+  public fps(): void {
+    let now = Date.now();
+    let dt = now - this.lastUpdate;
+    this.lastUpdate = now;
+
+    console.log(dt / 1000);
+
+    // calculates fps
+    const msec = Math.floor(Date.now() / 1000);
+    if (msec !== this.currentSecond) {
+      this.currentSecond = msec;
+      this.framesLastSecond = this.frameCount;
+      this.frameCount = 1;
+    } else { this.frameCount += 1; }
+
+    console.log(this.frameCount);
   }
 
   /**
    * Rendering of a frame
    */
   public renderFrame(): void {
+    this.getCanvasContext();
+    this.map.renderMap();
+    this.canvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.canvasContext.font = 'bold 10pt sans-serif';
+    this.canvasContext.fillStyle = '#ff0000';
+    this.canvasContext.fillText(`FPS: ${this.framesLastSecond}`, 10, 20);
+    this.fps();
 
+    requestAnimationFrame(() => this.renderFrame());
   }
 }
--- src/Map.ts
+++ src/Map.ts
@@ -7,13 +7,6 @@ export default class Map {
 
   private static readonly mapH = 22;
 
-  private currentSecond = 0;
-
-  private framesLastSecond = 0;
-
-  private frameCount = 0;
-
-  private lastUpdate = Date.now();
 
   private gameMap = [
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -42,70 +35,37 @@ export default class Map {
 
   private canvas: HTMLCanvasElement;
 
+  private canvasContext : CanvasRenderingContext2D;
+
   /**
    * Construc a new instance of this class
    *
    * @param canvas the canvas to render on
+   * @param canvasContext
    */
-  public constructor(canvas: HTMLElement) {
+  public constructor(canvas: HTMLElement, canvasContext: CanvasRenderingContext2D) {
     this.canvas = <HTMLCanvasElement>canvas;
-    // Resize the canvas to full window size
-    this.canvas.width = window.innerWidth - 1;
-    this.canvas.height = window.innerHeight - 4;
-    requestAnimationFrame(() => this.renderMap());
+    this.canvasContext = this.canvas.getContext('2d');
   }
 
   // eslint-disable-next-line jsdoc/require-returns
   /**
    * Draws the game to the canvas
    */
-  public renderMap(): number {
-    // Get the canvas rendering context
-    const ctx = this.canvas.getContext('2d');
-    // Clear the entire canvas
-    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-    ctx.font = 'bold 10pt sans-serif';
-
-
-    let now = Date.now();
-    let dt = now - this.lastUpdate;
-    this.lastUpdate = now;
-
-
-
-    console.log(dt / 1000);
-
-    // calculates fps
-    const msec = Math.floor(Date.now() / 1000);
-    if (msec !== this.currentSecond) {
-      this.currentSecond = msec;
-      this.framesLastSecond = this.frameCount;
-      this.frameCount = 1;
-    } else { this.frameCount += 1; }
-
-    console.log(this.frameCount);
-
+  public renderMap(): void {
     // runs array and displays it on map
     for (let y = 0; y < Map.mapH; y++) {
       for (let x = 0; x < Map.mapW; x++) {
         switch (this.gameMap[((y * Map.mapW) + x)]) {
           case 1:
-            ctx.fillStyle = '#999999';
+            this.canvasContext.fillStyle = '#999999';
             break;
           default:
-            ctx.fillStyle = '#eeeeee';
+            this.canvasContext.fillStyle = '#eeeeee';
         }
-        ctx.fillRect(x * Map.tileW, y * Map.tileH, Map.tileW, Map.tileH);
+        this.canvasContext.fillRect(x * Map.tileW, y * Map.tileH, Map.tileW, Map.tileH);
       }
     }
-console.log('works');
-    // display FPS rate
-    ctx.fillStyle = '#ff0000';
-    ctx.fillText(`FPS: ${this.framesLastSecond}`, 10, 20);
-
-    requestAnimationFrame(() => this.renderMap());
-
-    return (1);
+    console.log('works');
   }
 }
