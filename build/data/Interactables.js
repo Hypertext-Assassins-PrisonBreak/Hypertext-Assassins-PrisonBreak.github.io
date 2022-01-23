87e0090bc403c1a3f251c542120c5e25ce873175
Zapplightforce <pers0010@hz.nl>
2022-01-23 17:09:14 +0100

    l1

--- build/data/Interactables.js
+++ build/data/Interactables.js
@@ -16,6 +16,13 @@ export default class Interactables {
                 Questions.questionsNL.get('question1'),
                 Questions.questionsNL.get('question1')
             ])],
+        ['interactable5', new Interactable(6, 3, [
+                Questions.questionsEN.get('question1'),
+                Questions.questionsEN.get('question1')
+            ], [
+                Questions.questionsNL.get('question1'),
+                Questions.questionsNL.get('question1')
+            ])],
         ['interactable2', new Interactable(7, 7, [
                 Questions.questionsEN.get('question1'),
                 Questions.questionsEN.get('question1')
@@ -31,13 +38,6 @@ export default class Interactables {
                 Questions.questionsNL.get('question1')
             ])],
         ['interactable4', new Interactable(12, 7, [
-                Questions.questionsEN.get('question1'),
-                Questions.questionsEN.get('question1')
-            ], [
-                Questions.questionsNL.get('question1'),
-                Questions.questionsNL.get('question1')
-            ])],
-        ['interactable5', new Interactable(6, 3, [
                 Questions.questionsEN.get('question1'),
                 Questions.questionsEN.get('question1')
             ], [
--- src/data/Interactables.ts
+++ src/data/Interactables.ts
@@ -2,7 +2,7 @@ import Questions from './Questions.js';
 import Interactable from '../types/Interactable.js';
 
 export default class Interactables {
-// Array of all Interactable instances
+  // Array of all Interactable instances
   public interactables: Map<string, Interactable> = new Map<string, Interactable>([
     ['interactable0', new Interactable(1, 7,
       [
@@ -13,6 +13,14 @@ export default class Interactables {
         Questions.questionsNL.get('question0')])],
 
     ['interactable1', new Interactable(2, 1,
+      [
+        Questions.questionsEN.get('question1'),
+        Questions.questionsEN.get('question1')],
+      [
+        Questions.questionsNL.get('question1'),
+        Questions.questionsNL.get('question1')])],
+
+    ['interactable5', new Interactable(6, 3,
       [
         Questions.questionsEN.get('question1'),
         Questions.questionsEN.get('question1')],
@@ -37,14 +45,6 @@ export default class Interactables {
         Questions.questionsNL.get('question1')])],
 
     ['interactable4', new Interactable(12, 7,
-      [
-        Questions.questionsEN.get('question1'),
-        Questions.questionsEN.get('question1')],
-      [
-        Questions.questionsNL.get('question1'),
-        Questions.questionsNL.get('question1')])],
-
-    ['interactable5', new Interactable(6, 3,
       [
         Questions.questionsEN.get('question1'),
         Questions.questionsEN.get('question1')],
